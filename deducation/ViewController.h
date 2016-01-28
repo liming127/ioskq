1478b8af098e439cd5c9b1e6420d959b13001c40
mingli <mingli@minglidemac.local>
2016-01-28 04:20:59 -0800

    Initial Commit

---
+++ deducation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  deducation
+//
+//  Created by mingli on 16/1/28.
+//  Copyright © 2016年 miner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
